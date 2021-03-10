c10b0572f3ec374bb3a1eafa0e56648b85e9491a
Ederson-Branco <52886734+Ederson-Branco@users.noreply.github.com>
2021-03-10 10:49:04 -0300

    m

--- Desenvolvimentos/pdf_to_excel/app.py
+++ Desenvolvimentos/pdf_to_excel/app.py
@@ -8,8 +8,8 @@ from io import BytesIO
 def upload_arquivo():
     data = st.file_uploader('Escolha o arquivo', type = 'pdf')
     if data is not None:
-        df = tabula.read_pdf(data,pages="all",guess=False)
-        df1 = pd.DataFrame(df)
+        #df = tabula.read_pdf(data,pages="all",guess=False)
+        df1 = pd.DataFrame(data)
         return df1
 
 def filtra_linhas_nao_vazias(df,serie,index):
