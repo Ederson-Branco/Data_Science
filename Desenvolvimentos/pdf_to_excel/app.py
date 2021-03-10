e7c1c43f0b8a769891d6470facc992aa543218cc
Ederson-Branco <52886734+Ederson-Branco@users.noreply.github.com>
2021-03-10 10:51:32 -0300

    m

--- Desenvolvimentos/pdf_to_excel/app.py
+++ Desenvolvimentos/pdf_to_excel/app.py
@@ -151,10 +151,11 @@ def main():
 
     if df_pdf is not None:
         if st.button('Converter'):
-            df = cria_dataframe(df_pdf)
-            st.dataframe(df.head())
+            pd.DataFrame(df_pdf)
+            #df = cria_dataframe(df_pdf)
+            #st.dataframe(df.head())
 
-            st.markdown(download_link(df), unsafe_allow_html=True)
+            #st.markdown(download_link(df), unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
